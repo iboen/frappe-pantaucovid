3491cc6ce5b32be145cd788319a51b1f78a13338
Ibnu SW <punyaibun@gmail.com>
2021-07-08 17:16:05 +0700

    rename to patien

---
+++ pantaucovid/pantau_covid/doctype/patient/patient.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2021, Sinawardi and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class Patient(Document):
+	pass
---
+++ pantaucovid/pantau_covid/doctype/patient/test_patient.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2021, Sinawardi and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class TestPatient(unittest.TestCase):
+	pass
